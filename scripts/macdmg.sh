eee7823d50ac9bb3f51fd89f1696431dffd74a4c
Mikael Sundell <mikael.sundell@gmail.com>
2024-01-02 21:19:32 +0100

    Updated about dialog Added copyright info Fixed macdmg Fixed MacOSXBundle.plist

--- scripts/macdmg.sh
+++ scripts/macdmg.sh
@@ -92,7 +92,7 @@ function deploy_dmg() {
 
     # dsstore
     # must be created with the same id, use temporary dmg to modify and copy afterwards
-    resources_dir=$(dirname ${script})/../sources/colorman/resources
+    resources_dir=$(dirname ${script})/../resources
     cp "${resources_dir}/DS_Store" "$dmg_volume/.DS_Store"
 
     # applications
