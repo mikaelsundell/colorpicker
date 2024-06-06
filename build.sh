972085c4504a8fd310c5c9275df667369ff66e5b
Mikael Sundell <mikael.sundell@gmail.com>
2024-06-06 14:15:38 +0200

    Added pkg deploy support

--- build.sh
+++ build.sh
@@ -13,6 +13,53 @@ sign_code=OFF
 code_sign_identity=""
 development_team_id=""
 
+# Create distribution.xml for the pkg file
+create_distribution_xml() {
+    local identifier="$1"
+    local version="$2"
+    local pkg_file="$3"
+
+    cat <<EOF > distribution.xml
+<?xml version="1.0" encoding="utf-8"?>
+<installer-gui-script minSpecVersion="1">
+    <title>Colorpicker</title>
+    <options customize="never" require-scripts="false"/>
+    <domains enable_anywhere="false" enable_currentUserHome="false" enable_localSystem="true"/>
+    <volume-check>
+        <allowed-os-versions>
+            <os-version min="10.12"/>
+        </allowed-os-versions>
+    </volume-check>
+    <installation-check script="installerCheckScript()"/>
+    <script>
+        <![CDATA[
+        function installerCheckScript() {
+            if (system.version.ProductVersion < '10.12') {
+                my.result.title = 'Unsupported macOS version';
+                my.result.message = 'This package requires macOS 10.12 or later.';
+                my.result.type = 'Fatal';
+                return false;
+            }
+            return true;
+        }
+        ]]>
+    </script>
+    <choices-outline>
+        <line choice="default">
+            <line choice="application"/>
+        </line>
+    </choices-outline>
+    <choice id="default" title="Colorpicker">
+        <pkg-ref id="$identifier"/>
+    </choice>
+    <choice id="application" visible="false">
+        <pkg-ref id="$identifier"/>
+    </choice>
+    <pkg-ref id="$identifier" version="$version" onConclusion="none">$pkg_file</pkg-ref>
+</installer-gui-script>
+EOF
+}
+
 # check signing
 parse_args() {
     while [[ "$#" -gt 0 ]]; do
@@ -23,6 +70,8 @@ parse_args() {
                 sign_code=ON ;;
             --deploy)
                 deploy=ON ;;
+            --pkg)
+                pkg=ON ;;
             *)
                 build_type="$1" # save it in build_type if it's not a recognized flag
                 ;;
@@ -48,8 +97,8 @@ if [ "$sign_code" == "ON" ]; then
     read -p "Enter Code Sign Identity [$default_code_sign_identity]: " input_code_sign_identity
     code_sign_identity=${input_code_sign_identity:-$default_code_sign_identity}
 
-    if [[ ! "$code_sign_identity" == *"Developer ID"* ]]; then
-        echo "Code Sign identity must contain 'Developer ID' to be a valid developer certificate."
+    if [[ ! "$code_sign_identity" == *"Developer ID"* ]] && [[ ! "$code_sign_identity" == *"3rd Party Mac Developer"* ]]; then
+        echo "Code Sign identity must contain 'Developer ID' or '3rd Party Mac Developer' to be a valid certificate."
         exit 1
     fi
 
@@ -119,10 +168,9 @@ build_colorpicker() {
     xcode_type=$(echo "$build_type" | awk '{ print toupper(substr($0, 1, 1)) tolower(substr($0, 2)) }')
 
     # build
-    cmake .. -DCMAKE_MODULE_PATH="$script_dir/modules" -DCMAKE_PREFIX_PATH="$prefix" -G Xcode &&
+    cmake .. -DCMAKE_MODULE_PATH="$script_dir/modules" -DCMAKE_PREFIX_PATH="$prefix" -G Xcode && 
+    cmake --build . --config $xcode_type --parallel &&
     if [ "$deploy" == "ON" ]; then
-        cmake --build . --config $xcode_type --parallel
-
         dmg_file="$script_dir/Colorpicker_macOS${major_version}_${machine_arch}_${build_type}.dmg"
         if [ -f "$dmg_file" ]; then
             rm -f "$dmg_file"
@@ -138,6 +186,39 @@ build_colorpicker() {
         $script_dir/scripts/macdmg.sh -b "$xcode_type/Colorpicker.app" -d "$dmg_file"
         if [ "$sign_code" == "ON" ]; then
             codesign --force --deep --sign "$code_sign_identity" --timestamp --options runtime --verbose "$dmg_file"
+        fi
+    fi
+
+    if [ "$pkg" == "ON" ]; then
+        pkg_file="$script_dir/Colorpicker_macOS${major_version}_${machine_arch}_${build_type}.pkg"
+        if [ -f "$pkg_file" ]; then
+            rm -f "$pkg_file"
+        fi
+
+        # deploy
+        $script_dir/scripts/macdeploy.sh -b "$xcode_type/Colorpicker.app" -m "$prefix/bin/macdeployqt"
+        
+        # plist
+        plist_path="$xcode_type/Colorpicker.app/Contents/Info.plist"
+        if [ ! -f "$plist_path" ]; then
+            echo "pkg version and identifier could not be found, info.plist not found in $xcode_type/Colorpicker.app"
+            exit 1
+        fi
+        version=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$plist_path")
+        identifier=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$plist_path")
+
+        # create pkg
+        pkgbuild --component "$xcode_type/Colorpicker.app" --identifier "$identifier" --version "$version" --install-location "/Applications" "intermediate.pkg"
+
+        # Create distribution.xml
+        create_distribution_xml "$identifier" "$version" "intermediate.pkg"
+
+        # Use productbuild to create the final pkg
+        productbuild --distribution distribution.xml --package-path . "$pkg_file"
+
+        if [ "$sign_code" == "ON" ]; then
+            productsign --sign "$code_sign_identity" "$pkg_file" "$pkg_file.signed"
+            mv "$pkg_file.signed" "$pkg_file"
         fi
     fi
 }
